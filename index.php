409f5ac20b0dd16cad01a77b8e88e7227d3edf3a
albert199577 <mso729049@gamil.com>
2021-12-27 10:26:25 +0800

    login, logout easy version

--- index.php
+++ index.php
@@ -26,8 +26,31 @@
 				<div id="lf" style="float:left;">
 					<div id="menuput" class="dbor">
 					<!--主選單放此-->
-													<span class="t botli">主選單區</span>
-												</div>
+						<span class="t botli">主選單區</span>
+						<?php
+							$mains = $Menu -> all(["parent" => 0, "sh" => 1]);
+
+							foreach ($mains as $key => $value) {
+								echo "<div class='mainmu'>";
+								echo "<a href='{$value['href']}'>";
+								echo $value['name'];
+								echo "</a>";
+								if ($Menu -> math("count", "*" , ["parent" => $value["id"]])) {
+									$subs = $Menu -> all(["parent" => $value["id"]]);
+									echo "<div class='mw'>";
+									foreach ($subs as $key => $value) {
+										echo "<div class='mainmu2'>";
+										echo "<a href='{$value['href']}'>{$value['name']}</a>";
+										echo "</div>";
+									}
+									echo "</div>";
+								}
+									echo "</div>";
+							}
+						?>
+
+
+					</div>
 					<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
 						<span class="t">進站總人數 : <?= $Total->find(1)['total']?></span>
 					</div>
@@ -52,7 +75,7 @@
 				
 								<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
 					<!--右邊-->   
-					<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;back.php&#39;)">管理登入</button>
+					<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
 					<div style="width:89%; height:480px;" class="dbor" style="dip">
 						<span class="t botli">校園映象區</span>
 							<div class="t" style="padding: 0" onclick="pp(1)"><img src="./icon/up.jpg" alt="" class=""></div>
@@ -61,19 +84,17 @@
 								foreach ($imgs as $key => $value) {		
 							?>
 							
-							<div class="t im cent" id="ssaa<?=$key;?>">
-								<img src="img/<?=$value['img'];?>" alt="" width="100px" heigh="100px" style="border: solid 1px orange">
+							<div class="im cent t" id="ssaa<?=$key;?>">
+								<img src="img/<?=$value['img'];?>" width="100px" heigh="100px" style="border: solid 1px orange">
 							</div>
 							<?php
-							}				
+							}
 							?>
 
 							<div class="t" style="padding: 0" onclick="pp(2)"><img src="./icon/dn.jpg" alt="" class=""></div>
-
-
-
 						<script>
 							var nowpage = 0, num = <?=$Image -> math("count", "*", ['sh' => 1]);?>;
+							
 							function pp(x) {
 								var s, t;
 								if (x == 1 && nowpage - 1 >= 0) {
@@ -92,7 +113,7 @@
 						</script>
 					</div>
 				</div>
-							</div>
+				</div>
 				<div style="clear:both;"></div>
 				<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
 					<span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom']?></span>
